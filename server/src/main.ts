3cf08d2736543b6199413e8c2b0fffbc45a61343
d.rakhmanzhanov <dastan.rakhmanzhanov@timelysoft.net>
2023-03-10 12:07:11 +0600

    Enable CORS and File Module

--- server/src/main.ts
+++ server/src/main.ts
@@ -5,6 +5,7 @@ const start = async () => {
   try {
     const PORT = process.env.PORT || 5000;
     const app = await NestFactory.create(AppModule);
+    app.enableCors()
     await app.listen(PORT, () => {
       console.log("App is listening port ", PORT)
     })
